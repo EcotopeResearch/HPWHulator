9d51568fbd4ef802e8ca3e087a15524e52f5b722
pmskintner <50156325+pmskintner@users.noreply.github.com>
2020-04-17 14:28:05 -0700

    New init functions and tests
    New variable defrostFactor to derate heating capacity to account for the defrost cycle
    Add many more tests
    Update all tests

--- HPWHsizer.py
+++ HPWHsizer.py
@@ -8,33 +8,83 @@ Created on Mon Apr 13 09:07:05 2020
 import numpy as np;
 
 class HPWHSizer:
-    rhoCp = 8.345;
+    rhoCp = 8.353535;
     W_TO_BTUHR = 3.412142;
+    W_TO_BTUMIN = W_TO_BTUHR/60;
     W_TO_TONS = 0.000284345;
     
     schematicNames = ["primary", "swingtank","tempmaint"];
     
     def __init__(self):
         """Initialize the sizer object with 0's for the inputs"""
-        self.nBR        = np.zeros(6); # Number of bedrooms 0Br, 1Br...
-        self.rBR        = np.zeros(6); # Ratio of people bedrooms 0Br, 1Br...
-        self.nPeople    = 0.; # Nnumber of people
-        self.gpdpp      = 0.; # Gallons per day per person
+        self.nBR            = np.zeros(6); # Number of bedrooms 0Br, 1Br...
+        self.rBR            = np.zeros(6); # Ratio of people bedrooms 0Br, 1Br...
+        self.nPeople        = 0.; # Nnumber of people
+        self.gpdpp          = 0.; # Gallons per day per person
         self.loadShapeNorm  = np.zeros(24); # The normalized load shape
-        self.supplyT    = 0.; # The supply temperature to the occupants
-        self.incomingT  = 0.; # The incoming cold water temperature for the city
-        self.storageT   = 0.; # The primary hot water storage temperature 
-        self.compRuntime  = 0.; # The runtime?
-        self.metered    = 0; # If the building as individual metering on the apartment or not
-        self.percentUseable = 0; #The  percent of useable storage
+        self.supplyT        = 0.; # The supply temperature to the occupants
+        self.incomingT      = 0.; # The incoming cold water temperature for the city
+        self.storageT       = 0.; # The primary hot water storage temperature 
+        self.compRuntime    = 0.; # The runtime?
+        self.metered        = 0; # If the building as individual metering on the apartment or not
+        self.percentUseable = 0; # The  percent of useable storage
+        self.aquaFract      = 0.; # The aquastat fraction
         
-        self.aquaFract = 0.; # The aquastat fraction
+        self.defrostFactor  = 1.; # The defrost factor. Derates the output power for defrost cycles.
+
+        self.schematic      = ""; # The schematic for sizing maybe just primary maybe with temperature maintenance.
+        self.swingOnT       = 0.; # The temperature the swing tank turns on at
+        self.nApt           = 0.; # The number of apartments
+        self.Wapt           = 0.; # The recirculation loop losses in terms of W/apt
+        self.fdotRecirc     = 0.; # The reciculation loop flow rate (gpm)
+        self.returnT        = 0.; # The reciculation loop return temperature (F)
+        self.TMRuntime      = 0.; # The temperature maintenance minimum runtime.
+        self.setpointTM     = 0.; # The setpoint of the temperature maintenance tank.
         
-        self.schematic  = ""; # The schematic for sizing maybe just primary maybe with temperature maintenance.
-        self.swingOnT   = 0.; # The temperature the swing tank turns on at
-        self.nApt       = 0.; # The number of apartments
-        self.Wapt       = 0.; # The recirculation loop losses in terms of W/apt
-        self.TMRuntime  = 0.; # The temperature maintenance minimum runtime.
+        self.UAFudge        = 0.;
+        self.totalHWLoad    = 0.;
+        self.offTime        = 0.;
+        
+    def initByUnits(self, nBR, rBR, gpdpp, loadShapeNorm, supplyT, incomingT, 
+                    storageT, compRuntime, metered, percentUseable, aquaFract, defrostFactor, 
+                    schematic):
+        self.nBR            = np.array(nBR); # Number of bedrooms 0Br, 1Br...
+        self.rBR            = np.array(rBR); # Ratio of people bedrooms 0Br, 1Br...
+        self.gpdpp          = gpdpp; # Gallons per day per person
+        self.loadShapeNorm  = np.array(loadShapeNorm); # The normalized load shape
+        self.supplyT        = supplyT; # The supply temperature to the occupants
+        self.incomingT      = incomingT; # The incoming cold water temperature for the city
+        self.storageT       = storageT; # The primary hot water storage temperature 
+        self.compRuntime    = compRuntime; # The runtime?
+        self.metered        = metered; # If the building as individual metering on the apartment or not
+        self.percentUseable = percentUseable; #The  percent of useable storage
+        
+        self.aquaFract      = aquaFract; # The aquastat fraction
+        self.defrostFactor  = defrostFactor; # The defrost factor. Derates the output power for defrost cycles.
+
+        self.schematic      = schematic; # The schematic for sizing maybe just primary maybe with temperature maintenance.
+        self.__calcedVariables()
+    
+    def initByPeople(self, nPeople, gpdpp, loadShapeNorm, supplyT, incomingT, 
+                    storageT, compRuntime, metered, percentUseable, aquaFract, defrostFactor,
+                    schematic, nApt):
+        self.nPeople        = nPeople;
+        self.gpdpp          = gpdpp; # Gallons per day per person
+        self.loadShapeNorm  = np.array(loadShapeNorm); # The normalized load shape
+        self.supplyT        = supplyT; # The supply temperature to the occupants
+        self.incomingT      = incomingT; # The incoming cold water temperature for the city
+        self.storageT       = storageT; # The primary hot water storage temperature 
+        self.compRuntime    = compRuntime; # The runtime?
+        self.metered        = metered; # If the building as individual metering on the apartment or not
+        self.percentUseable = percentUseable; #The  percent of useable storage
+        
+        self.aquaFract      = aquaFract; # The aquastat fraction
+        self.defrostFactor  = defrostFactor; # The defrost factor. Derates the output power for defrost cycles.
+        self.schematic      = schematic; # The schematic for sizing maybe just primary maybe with temperature maintenance.
+        
+        self.nApt           = nApt;
+        
+        self.__calcedVariables();
 
     def __calcedVariables(self):
         """ Calculate other variables needed."""
@@ -48,18 +98,49 @@ class HPWHSizer:
         self.totalHWLoad = self.gpdpp * self.nPeople;
         #self.totalLoadShape = self.loadShapeNorm * self.totalHWLoad;
         self.UAFudge = 3;
-        
         self.offTime = sum(np.append(self.loadShapeNorm,self.loadShapeNorm)[22:36] < 1./48.) #The overnight design for off time. 
      
+    def setRecircVars(self, Wapt, returnT, fdotRecirc):
+        """Takes the recirc variables and solves for one that's set to zero"""
+        if any(x < 0 for x in [Wapt, returnT, fdotRecirc]):
+            raise Exception("All recirculation variables must be postitive.")
+        if self.supplyT <= returnT:
+            raise Exception("The return temperature is greater than the supply temperature! This sizer doesn't support heat trace on the recirculation loop")
+        if Wapt == 0:
+            self.Wapt = self.rhoCp / self.nApt * fdotRecirc * (self.supplyT - returnT) / self.W_TO_BTUMIN;
+            self.returnT    = returnT;
+            self.fdotRecirc = fdotRecirc;
+        elif returnT == 0 and self.schematic == 'tempmaint':
+            self.Wapt    = Wapt
+            self.returnT = self.supplyT - Wapt * self.nApt *self.W_TO_BTUMIN / self.rhoCp / fdotRecirc;
+            self.fdotRecirc = fdotRecirc;
+        elif fdotRecirc == 0 and self.schematic == 'tempmaint':
+            self.Wapt       = Wapt;
+            self.returnT    = returnT;
+            self.fdotRecirc = Wapt * self.nApt * self.W_TO_BTUMIN / self.rhoCp / (self.supplyT - returnT);
+        else:
+            raise Exception("In setting the recirculation variables one needs to be zero to solve for it.")
+        
+    def setTMVars(self, TMRuntime, setpointTM, Wapt, returnT, fdotRecirc):
+        self.TMRuntime = TMRuntime;
+        self.setpointTM = setpointTM;
+        self.setRecircVars(Wapt, returnT, fdotRecirc);
+        
+    def setSwingVars(self, swingOnT, Wapt):
+        self.swingOnT = swingOnT;
+        self.Wapt = Wapt;      
+        
+# The meat of the script        
     def primaryHeatHrs2kBTUHR(self, heathours):
         """Returns the heating capacity in kBTU/hr for the heating hours given by, heathours"""
-        heatCap = self.totalHWLoad / heathours * self.rhoCp * (self.storageT - self.incomingT)/1000.;
+        heatCap = self.totalHWLoad / heathours * self.rhoCp * \
+            (self.storageT - self.incomingT) / self.defrostFactor /1000.;
         return heatCap;
     
     def sizePrimaryTankVolume(self, heatHrs):
         """Sizes the primary HPWH plant with the new methodology"""
         if heatHrs <= 0 or heatHrs > 24:
-            raise Exception("The heating capacity scaled to hours is invalid, value is "+ heatCapHrs)
+            raise Exception("The heating capacity scaled to hours is invalid, value is "+ heatHrs)
             
         diffN = 1/heatHrs - np.append(self.loadShapeNorm,self.loadShapeNorm); 
         diffN = np.cumsum(diffN[np.argmax(diffN < 0.):]); #Get the rest of the day from the start of the peak
@@ -67,7 +148,8 @@ class HPWHSizer:
         runningVol = -min(np.append(diffN[diffN<0.], -0.)); #Minimum value less than 0 or 0.
         totalVol = runningVol / (1-self.aquaFract) / self.percentUseable;
         
-        return totalVol * self.totalHWLoad * (self.supplyT - self.incomingT) / (self.storageT - self.incomingT);
+        return totalVol * self.totalHWLoad * (self.supplyT - self.incomingT) / \
+            (self.storageT - self.incomingT);
 
     def sizeSystem(self):
         """ Size system based on schemtic """    
@@ -89,19 +171,21 @@ class HPWHSizer:
             volN[ii] = self.sizePrimaryTankVolume(heatHours[ii]);
         return [volN, self.primaryHeatHrs2kBTUHR(heatHours)]
 
-    
     def sizeSwing(self):
-        self.TMVol = (self.Wapt +self.UAFudge) * self.nApt / self.rhoCp * self.W_TO_BTUHR * self.offTime/(self.storageT - self.swingOnT);
-        self.TMCap = 2 * (self.Wapt +self.UAFudge) * self.nApt / 1000.;  ####Factor of TWO?!??!
+        self.TMVol = (self.Wapt + self.UAFudge) * self.nApt / self.rhoCp * \
+            self.W_TO_BTUHR * self.offTime / (self.storageT - self.swingOnT);
+        self.TMCap = 1.5 * (self.Wapt + self.UAFudge) * self.nApt * self.W_TO_BTUHR / 1000.;  #FACTOR OF 1.5!?!?
         
     def sizeTemperatureMaintenance(self):####FIX
-        self.TMCap =  100;####FIX
-        self.TMVol =  100;####FIX
+        minRunTime = 1; # Hour
+        self.TMCap =  24./self.TMRuntime * (self.Wapt + self.UAFudge) * self.nApt * self.W_TO_BTUHR / 1000.; #should we have this factor
+        self.TMVol =  (self.Wapt + self.UAFudge) * self.nApt / self.rhoCp * \
+            self.W_TO_BTUHR * minRunTime / (self.setpointTM - self.returnT);
         ###FINISH
        
 # Helper Functions for reading and writing files 
     def __importArrLine(self, line, setLength):
-        """Imports an array in line with a set lenght, setLength"""
+        """Imports an array in line with a set length, setLength"""
         val = np.zeros(setLength);
         if len(line) > setLength: 
                 raise Exception( '\nERROR: Too many data points given for loadShapeNorm, should be 24 but received '+ str(len(line)-1)+'.\n')  
@@ -110,18 +194,12 @@ class HPWHSizer:
                 val[ii-1]  = float(line[ii]);
             except IndexError:
                 raise Exception('\nERROR: Not enough data points given for loadShapeNorm, should be 24 but received '+str(ii)+'.\n') 
-        return val                
-    def __writeAttrLine(self, file, attrList):
-        for field in attrList:
-            var = getattr(self, field);
-            if isinstance(var, np.ndarray):
-                file.write(field + ', ' + np.array2string(var, precision = 4, separator=",") + '\n')
-            else:
-                file.write(field + ', ' + str(var) + '\n')
-                
+        return val         
+
     def initializeFromFile(self, fileName):
         """"Read in a formated file with filename"""
-
+        
+        Wapt = 0.; returnT = 0.; fdotRecirc = 0.;
         # Using readlines() 
         file1 = open(fileName, 'r');
         fileLines = file1.read().splitlines();     
@@ -149,6 +227,7 @@ class HPWHSizer:
                 self.compRuntime  = float(temp[1]);
             elif temp[0] == "metered":
                 self.metered    =  int(temp[1]); # If the building as individual metering on the apartment or not
+                
             elif temp[0] == "percentuseable":
                 temp[1] = float(temp[1])
                 if temp[1] > 1: # Check to make sure the percent is stored as anumber 0 to 1.
@@ -158,8 +237,13 @@ class HPWHSizer:
                 temp[1] = float(temp[1])
                 if temp[1] > 1: # Check to make sure the percent is stored as anumber 0 to 1.
                     temp[1] = temp[1]/100.
-                self.percentUseable = temp[1];
-            
+                self.aquaFract = temp[1];
+            elif temp[0] == "defrostfactor":
+                temp[1] = float(temp[1])
+                if temp[1] > 1: # Check to make sure the percent is stored as anumber 0 to 1.
+                    temp[1] = temp[1]/100.
+                self.defrostFactor = temp[1];
+                
             elif temp[0] == "schematic":
                 if temp[1] in self.schematicNames:
                     self.schematic  = temp[1];
@@ -169,38 +253,59 @@ class HPWHSizer:
                 self.swingOnT   = float(temp[1]);
             elif temp[0] == "napt":
                 self.nApt       = float(temp[1]);
-            elif temp[0] == "wapt":
-                self.Wapt       = float(temp[1]);
+                
             elif temp[0] == "tmruntime":
                 self.TMRuntime  = float(temp[1]);
+            elif temp[0] == "setpointtm":
+                self.setpointTM  = float(temp[1]);
+                  
+            elif temp[0] == "wapt":
+                Wapt      = float(temp[1]);
+            elif temp[0] == "returnt":    
+                returnT      = float(temp[1]);
+            elif temp[0] == "fdotrecirc":    
+                fdotRecirc      = float(temp[1]);
             else:
                 raise Exception('\nERROR: Invalid input given: '+ line +'.\n')
-                
+        # End for loop reading file lines.    
         self.__calcedVariables()
-
-           
-    def writeOutput(self, fileName):        
-        varListOut = ['totalHWLoad', 'UAFudge', 'offTime', 'primaryHeatingRate',
+        if self.schematic == 'tempmaint':
+            self.setRecircVars(Wapt, returnT, fdotRecirc)
+        elif self.schematic == 'swingtank':
+            self.Wapt = Wapt;
+            
+    def __writeAttrLine(self, file, attrList):
+        for field in attrList:
+            var = getattr(self, field);
+            if isinstance(var, np.ndarray):
+                file.write(field + ', ' + np.array2string(var, precision = 4, separator=",", max_line_width = 300.) + '\n');
+            elif isinstance(var, float):
+                file.write('%s, %5.3f \n' %( field ,var));
+            else:
+                file.write( field + ', ' + str(var) + '\n');
+                      
+    def writeOutput(self, fileName):  
+        """Writes the output to a file with file name filename."""
+        varListOut = ['totalHWLoad','offTime', 'primaryHeatingRate',
                       'primaryHeatCap','primaryVol', 'TMCap', 'TMVol'];
         
         varListIn = ['nPeople', 'gpdpp', 'loadShapeNorm', 'storageT','supplyT', 
                       'incomingT', 'compRuntime','metered', 'percentUseable', 
-                     'aquaFract',
-                     'schematic', 'swingOnT','nApt','Wapt',  'TMRuntime'];
+                     'aquaFract', 'defrostFactor', 'schematic',
+                     'nApt', 'Wapt', 'returnT', 'fdotRecirc', 'UAFudge',
+                     'swingOnT', 'setpointTM', 'TMRuntime'];
         if sum(self.nBR*self.rBR) > 0:    
              ['nBR', 'rBR'] + varListIn
-            
-       
              
         with open(fileName, 'w+') as file1:
             file1.write('OUTPUTS\n')
             self.__writeAttrLine(file1, varListOut)
             pCurve = self.primaryCurve();
             file1.write('primaryCurve_vol, ')
-            file1.write(np.array2string(pCurve[0], precision = 6, separator=","));
+            file1.write(np.array2string(pCurve[0], precision = 2, separator=",", max_line_width = 300.));
             file1.write('\n')
             file1.write('primaryCurve_heatCap, ')
-            file1.write(np.array2string(pCurve[1], precision = 2, separator=","));
+            file1.write(np.array2string(pCurve[1], precision = 2, separator=",", max_line_width = 300.));
             file1.write('\n\n')
             file1.write('INPUTS\n')
             self.__writeAttrLine(file1, varListIn)
--- tests/test_sizer.py
+++ tests/test_sizer.py
@@ -14,30 +14,115 @@ def empty_sizer():
     '''Returns a HPWHsizer instance initialized to zeros'''
     return HPWHSizer.HPWHSizer()
 
-def test_default_initial_amount(empty_sizer):
-    assert (empty_sizer.nBR  == np.zeros(6)).all()
-    assert (empty_sizer.rBR  == np.zeros(6)).all()
-    assert empty_sizer.nPeople    == 0.; # Nnumber of people
-    assert empty_sizer.gpdpp      == 0.; # Gallons per day per person
-    assert (empty_sizer.loadShapeNorm  == np.zeros(24)).all(); # The normalized load shape
-    assert empty_sizer.supplyT    == 0.; # The supply temperature to the occupants
-    assert empty_sizer.incomingT  == 0.; # The incoming cold water temperature for the city
-    assert empty_sizer.storageT   == 0.; # The primary hot water storage temperature 
-    assert empty_sizer.compRuntime  == 0.; # The runtime?
-    assert empty_sizer.metered    == 0; # If the building as individual metering on the apartment or not
-    assert empty_sizer.percentUseable == 0; #The  percent of useable storage
-    assert empty_sizer.aquaFract == 0.; # The aquastat fraction
+@pytest.fixture
+def units_sizer():
+    '''Returns a HPWHsizer instance initialized to with by units'''
+    hpwh = HPWHSizer.HPWHSizer()
+    hpwh.initByUnits( [50,50,50,50,0,0], [1.374,1.74,2.567,3.109,4.225,3.769], 20, 
+                     [0.027,0.013,0.008,0.008,0.024,0.04 ,0.074,0.087,\
+                      0.082,0.067,0.04 ,0.034, 0.034,0.029,0.027,0.029,\
+                      0.035,0.04 ,0.048,0.051,0.055,0.059,0.051,0.038, 0.],
+                    120, 50, 150, 16, 0, 0.8, .33, .9,'tempmaint')
+    hpwh.setTMVars(16, 135, 100., 117., 0);
+    return hpwh;
 
-    assert empty_sizer.schematic  == ""; # The schematic for sizing maybe just primary maybe with temperature maintenance.
-    assert empty_sizer.swingOnT   == 0.; # The temperature the swing tank turns on at
-    assert empty_sizer.nApt       == 0.; # The number of apartments
-    assert empty_sizer.Wapt       == 0.; # The recirculation loop losses in terms of W/apt
-    assert empty_sizer.TMRuntime  == 0.; # The temperature maintenance minimum runtime.
+@pytest.fixture
+def people_sizer():
+    '''Returns a HPWHsizer instance initialized by nPeople inputs'''
+    hpwh = HPWHSizer.HPWHSizer()
+
+    hpwh.initByPeople(100, 22., 
+                      [0.027,0.013,0.008,0.008,0.024,0.04 ,0.074,0.087,\
+                       0.082,0.067,0.04 ,0.034, 0.034,0.029,0.027,0.029,\
+                       0.035,0.04 ,0.048,0.051,0.055,0.059,0.051,0.038, 0.],
+                    120, 50, 150., 18., 0, .8, .3,.9, "swingtank", 36);
+    hpwh.setSwingVars(122., 100.);
+    return hpwh;
+# End of fixtures
+
+# Start of tests
+def test_default_initial_amount(empty_sizer):
+    assert (empty_sizer.nBR             == np.zeros(6)).all()
+    assert (empty_sizer.rBR             == np.zeros(6)).all()
+    assert empty_sizer.nPeople          == 0.; # Nnumber of people
+    assert empty_sizer.gpdpp            == 0.; # Gallons per day per person
+    assert (empty_sizer.loadShapeNorm   == np.zeros(24)).all(); # The normalized load shape
+    assert empty_sizer.supplyT          == 0.; # The supply temperature to the occupants
+    assert empty_sizer.incomingT        == 0.; # The incoming cold water temperature for the city
+    assert empty_sizer.storageT         == 0.; # The primary hot water storage temperature 
+    assert empty_sizer.compRuntime      == 0.; # The runtime?
+    assert empty_sizer.metered          == 0; # If the building as individual metering on the apartment or not
+    assert empty_sizer.percentUseable   == 0; #The  percent of useable storage
+    assert empty_sizer.aquaFract        == 0.; # The aquastat fraction
+    assert empty_sizer.defrostFactor    == 1.; # The defrost factor. Derates the output power for defrost cycles.
+
+    assert empty_sizer.schematic        == ""; # The schematic for sizing maybe just primary maybe with temperature maintenance.
+    assert empty_sizer.swingOnT         == 0.; # The temperature the swing tank turns on at
+    assert empty_sizer.nApt             == 0.; # The number of apartments
+    assert empty_sizer.Wapt             == 0.; # The recirculation loop losses in terms of W/apt
+    assert empty_sizer.TMRuntime        == 0.; # The temperature maintenance minimum runtime.
+    assert empty_sizer.UAFudge          == 0.;
+    assert empty_sizer.totalHWLoad      == 0.;
+    assert empty_sizer.offTime          == 0.;
+    assert empty_sizer.Wapt             == 0.;
+    assert empty_sizer.returnT          == 0.;
+    assert empty_sizer.fdotRecirc       == 0.;
     
+    with pytest.raises(Exception):
+        assert empty_sizer.sizeSystem();
+        assert empty_sizer.sizePrimaryTankVolume(-10)
+        assert empty_sizer.sizePrimaryTankVolume(100)
+
+def test_people_sizer(people_sizer):
+    with pytest.raises(Exception):
+        assert people_sizer.sizePrimaryTankVolume(-10)
+        assert people_sizer.sizePrimaryTankVolume(100)
+    people_sizer.sizeSystem();
+    people_sizer.writeOutput("tests/output/people_sizer.txt");
+    assert file_regression("tests/ref/people_sizer.txt", 
+                           "tests/output/people_sizer.txt");
+def test_units_sizer(units_sizer):
+    with pytest.raises(Exception):
+        assert units_sizer.sizePrimaryTankVolume(-10)
+        assert units_sizer.sizePrimaryTankVolume(100)
+    units_sizer.sizeSystem();
+    units_sizer.writeOutput("tests/output/units_sizer.txt");
+    assert file_regression("tests/ref/units_sizer.txt", 
+                           "tests/output/units_sizer.txt");
+
+@pytest.mark.parametrize("hrs", [
+    20, 16, 12
+])
+def test_primaryHeatHrs2kBTUHR(empty_sizer, hrs):
+    assert empty_sizer.primaryHeatHrs2kBTUHR(hrs) == 0.;
+
+
+@pytest.mark.parametrize("Wapt, returnT, fdotRecirc", [
+    (99.15126, 117, 45), 
+    (58.75630029, 110, 8),
+    (881.3445044, 40, 15)
+])
+def test_setRecircVars(units_sizer, Wapt, returnT, fdotRecirc):
+    tol = 1e-4;
+    units_sizer.setRecircVars(0, returnT, fdotRecirc)
+    assert abs(units_sizer.Wapt - Wapt) < tol;
+    units_sizer.setRecircVars(Wapt, 0, fdotRecirc)   
+    assert abs(units_sizer.returnT - returnT) < tol;
+    units_sizer.setRecircVars(Wapt, returnT, 0)      
+    assert abs(units_sizer.fdotRecirc - fdotRecirc) < tol;
     
+    with pytest.raises(Exception):
+        assert units_sizer.setRecircVars(0, returnT, 0)
+        assert units_sizer.setRecircVars(-Wapt, -returnT, 0)
+        assert units_sizer.setRecircVars(Wapt, units_sizer.supplyTemp, 0)
+        assert units_sizer.setRecircVars(Wapt, units_sizer.supplyTemp+10, 0)
+
+
+    
+# File tests!
 @pytest.mark.parametrize("file1", [
     "tests/test_60UnitSwing.txt",
-    "tests/test_200UnitSwing.txt"
+    "tests/test_200UnitTM.txt"
 ])
 def test_hpwh_from_file(empty_sizer, file1):
     empty_sizer.initializeFromFile(file1);
@@ -46,4 +131,3 @@ def test_hpwh_from_file(empty_sizer, file1):
     
     assert file_regression("tests/ref/"+os.path.basename(file1), 
                            "tests/output/"+os.path.basename(file1));
-
