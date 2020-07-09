382e657872e541d77ae6e4789f7d4430da723682
pmskintner <50156325+pmskintner@users.noreply.github.com>
2020-07-08 18:30:20 -0700

    Update dataFetch.py

--- dataFetch.py
+++ dataFetch.py
@@ -24,3 +24,14 @@ class hpwhDataFetch():
         #cdf = list(zip(x, lognorm.cdf(x, params[0], params[1] + cdf_shift, params[2])))
         cdf = lognorm.cdf(x, params[0], params[1] + cdf_shift, params[2])
         return(cdf)
+    
+    def getLoadshape(self):
+        return self.dataDict['loadshapes']['Stream']
+    
+    
+    def getGPDPP(self, key):
+        try: 
+            return self.dataDict['gpdpp'][key]
+        except KeyError:
+            raise KeyError("Mapping key not found, valid keys are ashLow, ashMed, EcoMark, Ca")
+    
