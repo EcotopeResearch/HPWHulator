1ce978b428eacab18a51d9110b1fab4cee302220
pmskintner <50156325+pmskintner@users.noreply.github.com>
2020-07-20 12:33:33 -0700

    update setupt and requirements for pkg dist

--- setup.py
+++ setup.py
@@ -5,6 +5,12 @@ from setuptools import find_packages, setup
 setup(
     name='HPWHulator',
     version='0.0.1',
+
     packages=find_packages(),
-    include_package_data=True
+    include_package_data=True,
+    url="https://github.com/EcotopeResearch/HPWHulator",
+    description="A public Heat Pump Water Heater (HPWH) sizing calculator that uses the Ecotope Modiefied ASHRAE method and the ASHRAE method.",
+
+    python_requires='>=3.7',
+
 )
